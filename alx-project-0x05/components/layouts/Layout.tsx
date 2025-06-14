9c650e2e13775ec0c861a22334f35ed3bd7e5361
Joel Anarba <157653692+joelanarba@users.noreply.github.com>
2025-06-14 09:15:10 +0000

    Add Context API version of counter app

---
+++ alx-project-0x05/components/layouts/Layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { LayoutProps } from "@/interface";
+
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="pt-24">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
